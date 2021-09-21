0ecaf5b1945587996b9437ce6ccb5c01fe402ad4
Alex Ho <alexanderkho92@gmail.com>
2021-09-21 11:00:37 -0600

    better error handling

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,9 +3,14 @@ import { API_KEY } from "../secret";
 
 const API = "https://api.giphy.com/v1/gifs";
 
-const getGifs = (search) => {
+const getGifs = async (search) => {
   const url = `${API}/search?api_key=${API_KEY}&q=${search}&limit=20`;
-  return fetch(url).then((res) => res.json());
+  const res = await fetch(url);
+  if (!res.ok) {
+    const { message } = await res.json();
+    throw new Error(`${res.status} Error: ${message}`);
+  }
+  return res.json();
 };
 
 const appReducer = (state, action) => {
